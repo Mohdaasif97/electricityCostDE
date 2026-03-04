6f374d690d9b98dddb0c5ccfe43e0a441b0d2e5f
Aasif Mohd <122384084+Mohdaasif97@users.noreply.github.com>
2026-03-04 13:51:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -183,9 +183,18 @@ export default function Home() {
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+       <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+  rel="preload"
+  as="style"
+  href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap"
+  onLoad={(e) => { e.currentTarget.rel = 'stylesheet' }}
+/>
+<noscript>
+  <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+</noscript>
+  
       </Head>
 
       <div className={styles.page}>
